76c3853f73bcecf6daeaa3fb1fdd079a60ac6f9a
Christian Nygaard <cnygaard@redbridge.se>
2020-02-02 23:13:28 +0100

    * Move Grizzly to Grizzly folder * Move Netty to main Java * Maven pom.xml add Netty, Jetty dependancies

---
+++ src/main/java/AppServer.java
@@ -0,0 +1,45 @@
+//package com.okta.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class AppServer {
+
+    private static final int HTTP_PORT = 8080;
+
+    public void run() throws Exception {
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+
+            ServerBootstrap httpBootstrap = new ServerBootstrap();
+
+            httpBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ServerInitializer())
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            // Bind and start to accept incoming connections.
+            ChannelFuture httpChannel = httpBootstrap.bind(HTTP_PORT).sync();
+
+            // Wait until the server socket is closed
+            httpChannel.channel().closeFuture().sync();
+        }
+        finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new AppServer().run();
+    }
+
+}
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,29 @@
+//package org.example;
+
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(urlPatterns = {"/*"}, loadOnStartup = 1)
+public class HelloWorld extends HttpServlet
+{
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException
+    {
+        response.getOutputStream().print("Hello World");
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+}
+
+
+
+
